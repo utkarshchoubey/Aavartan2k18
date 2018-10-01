25becb46047f603c497219137e23b0d0182134bb
Amanjain15 <amanjain.1596@gmail.com>
2018-10-02 00:20:21 +0530

    Minor Changes to SplashScreen Module

--- app/src/main/java/com/technocracy/app/aavartan/activity/splash_screen/presenter/SplashScreenPresenterInter.java
+++ app/src/main/java/com/technocracy/app/aavartan/activity/splash_screen/presenter/SplashScreenPresenterInter.java
@@ -1,6 +1,5 @@
 package com.technocracy.app.aavartan.activity.splash_screen.presenter;
 
-public interface SplashScreenPresenter {
-
+public interface SplashScreenPresenterInter {
     void requestSplash();
 }
